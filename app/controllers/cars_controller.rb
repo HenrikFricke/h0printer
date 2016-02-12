3d9d0631056d12f0f0ef38399a77e701f12fe0cc
Henrik Fricke <henrik.fricke@jimdo.com>
2016-02-12 20:07:11 +0100

    actions to edit cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -10,10 +10,17 @@ class CarsController < ApplicationController
   def create
     @car = Car.new(car_params)
     if @car.save
-      redirect_to cars_path
+      redirect_to root_path
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+
+    redirect_to root_path
   end
 
   private
