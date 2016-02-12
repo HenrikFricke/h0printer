e85948060d0ec715f1ca450f0ef149009848faeb
Henrik Fricke <henrik.fricke@jimdo.com>
2016-02-12 16:17:31 +0100

    create model car and ressource cars

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160212151503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string   "name"
+    t.string   "mark"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
