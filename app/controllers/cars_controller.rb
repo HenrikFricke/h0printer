2a6772f5362991f5feba8333c24dd29e4488f868
Henrik Fricke <henrik.fricke@jimdo.com>
2016-02-12 17:06:11 +0100

    basic cars modelling

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,23 @@
 class CarsController < ApplicationController
   def index
+    @cars = Car.all
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to cars_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def car_params
+      params.require(:cars).permit(:name, :mark)
+    end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,4 @@
 class Car < ActiveRecord::Base
+  validates :name, presence: true
+  validates :mark, presence: true 
 end
